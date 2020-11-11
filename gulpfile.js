849e20bae89604dde834424d37f77299aef4f6e3
Nikola Horbach <44953799+NicolaHorbach@users.noreply.github.com>
2020-11-12 00:33:26 +0200

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -30,3 +30,41 @@ gulp.task("server", function() {
 });
 
 gulp.task("start", gulp.series("css", "server"));
+
+
+// let gulp = require('gulp');
+// let plumber = require('gulp-plumber');
+// let sourcemap = require('gulp-sourcemaps');
+// let sass = require('gulp-sass');
+// let postcss = require('gulp-postcss');
+// let autoprefixer = require('autoprefixer');
+// let server = require('browser-sync').create();
+
+// gulp.task('scss', function() {
+//   return gulp.src('dev/style/**/*.scss')
+//     .pipe(plumber())
+//     .pipe(sourcemap.init())
+//     .pipe(sass())
+//     .pipe(postcss())
+//     .pipe(autoprefixer({
+//       browsers: ['last 2 versions']
+//     }))
+//     .pipe(sourcemap.write("."))
+//     .pipe(gulp.dest(""))
+//     .pipe(server.stream());
+
+// })
+
+// gulp.task("server", function() {
+//   server.init({
+//     server: "dev/pages",
+//     notify: false,
+//     open: true,
+//     cors: true,
+//     ui: false
+//   });
+//   gulp.watch("source/styles/**/**/*.scss", gulp.series("css"));
+//   gulp.watch("source/*.html").on("change", server.reload);
+// });
+
+// gulp.task("start", gulp.series("css", "server"));
